6f1e0dca8b14083d08d55435104b4679ab6bddbd
p941260 <113489300+p941260@users.noreply.github.com>
2022-09-27 01:41:46 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,69 +1,111 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showNumber(input.temperature())
-    if (temperature >= 30) {
-        for (let index = 0; index < 2; index++) {
-            basic.showString("Very Hot")
-        }
-        basic.showString("T-Shirt")
-        music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once)
-    } else if (temperature >= 20) {
-        for (let index = 0; index < 2; index++) {
-            basic.showString("Hot")
-        }
-        basic.showString("MTM")
-    } else if (temperature >= 10) {
-        for (let index = 0; index < 2; index++) {
-            basic.showString("Cool")
-        }
-        basic.showString("Coat")
-    } else if (temperature >= 0) {
-        for (let index = 0; index < 2; index++) {
-            basic.showString("Cold")
-        }
-        basic.showString("Pad")
-    } else {
-        for (let index = 0; index < 2; index++) {
-            basic.showString("Very Cold")
-        }
-        music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once)
-    }
-    basic.pause(1000)
-    basic.clearScreen()
+input.onGesture(Gesture.TiltRight, function () {
+    music.setVolume(music.volume() + 20)
+    basic.showLeds(`
+        # # . . .
+        # . . # .
+        # . # # #
+        # . . # .
+        # # . . .
+        `)
 })
+function Bad () {
+    basic.showIcon(IconNames.Sad)
+    basic.pause(1000)
+    basic.showString("Bad Choice")
+}
+input.onGesture(Gesture.TiltLeft, function () {
+    music.setVolume(music.volume() - 20)
+    basic.showLeds(`
+        # # . . .
+        # . . . .
+        # . # # #
+        # . . . .
+        # # . . .
+        `)
+})
+function Temp () {
+    basic.showNumber(temperature)
+    if (temperature >= 28) {
+        music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once)
+        basic.showString("Very Hot")
+        basic.showString("Sleeveless Shirt")
+    } else if (temperature >= 17) {
+        basic.showString("Hot")
+        basic.showString("T-Shirt")
+    } else if (temperature >= 12) {
+        basic.showString("Cool")
+        basic.showString("MTM")
+    } else if (temperature >= 5) {
+        basic.showString("Cold")
+        basic.showString("Coat")
+    } else {
+        music.startMelody(music.builtInMelody(Melodies.Baddy), MelodyOptions.Once)
+        basic.showString("Very Cold")
+        basic.showString("Padding")
+    }
+    basic.showIcon(IconNames.Happy)
+}
+input.onButtonPressed(Button.A, function () {
+    Temp()
+})
+function Object_Classification () {
+    huskylens.initI2c()
+    huskylens.initMode(protocolAlgorithm.OBJECTCLASSIFICATION)
+    while (Num == 0) {
+        huskylens.request()
+        if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            if (temperature >= 28) {
+                Good()
+            } else {
+                Bad()
+            }
+        } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            if (28 > temperature && temperature >= 17) {
+                Good()
+            } else {
+                Bad()
+            }
+        } else if (huskylens.isAppear(3, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            if (17 > temperature && temperature >= 12) {
+                Good()
+            } else {
+                Bad()
+            }
+        } else if (huskylens.isAppear(4, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            if (12 > temperature && temperature >= 5) {
+                Good()
+            } else {
+                Bad()
+            }
+        } else if (huskylens.isAppear(5, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            if (5 > temperature) {
+                Good()
+            } else {
+                Bad()
+            }
+        } else if (huskylens.isAppear(0, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            basic.showIcon(IconNames.SmallDiamond)
+            basic.showIcon(IconNames.Diamond)
+            basic.showIcon(IconNames.Target)
+        }
+    }
+    basic.showString("Bye")
+}
 input.onGesture(Gesture.FreeFall, function () {
     for (let index = 0; index < 4; index++) {
         music.playMelody("C5 - C5 - C5 - C5 - ", 130)
     }
+    basic.showString("Hello!")
 })
-function Object_Recognition () {
-    huskylens.initI2c()
-    huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_RECOGNITION)
-    if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        basic.showLeds(`
-            . . # # #
-            . . . . .
-            . # # . .
-            . # # . #
-            . # # # .
-            `)
-        basic.pause(100)
-        basic.clearScreen()
-    } else if (huskylens.isAppear(2, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
-        basic.showLeds(`
-            . . . . .
-            # # # # .
-            . . . # .
-            . # # . .
-            . . . . .
-            `)
-        basic.pause(100)
-        basic.clearScreen()
-    }
-}
-input.onButtonPressed(Button.AB, function () {
-    Object_Recognition()
+input.onGesture(Gesture.LogoDown, function () {
+    music.setVolume(127)
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Forever)
+    basic.showString("enjoy~")
 })
-input.onButtonPressed(Button.B, function () {
+input.onGesture(Gesture.ScreenDown, function () {
+    Num = 1
+})
+function Light () {
     basic.showNumber(input.lightLevel())
     if (input.lightLevel() > 200) {
         basic.showLeds(`
@@ -71,32 +113,44 @@ input.onButtonPressed(Button.B, function () {
             . # # # .
             # # # # #
             . . . . .
-            . . . . .
+            . # . # .
             `)
         basic.showString("SunCream!")
     } else if (input.lightLevel() > 100) {
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
+        basic.showIcon(IconNames.Happy)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.soaring), SoundExpressionPlayMode.UntilDone)
     } else if (input.lightLevel() >= 0) {
         basic.showLeds(`
-            . # # # .
-            . . # # #
+            . . # # .
             . . . # #
-            . . # # #
+            # . # # #
+            # # # # #
             . # # # .
             `)
-        music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
+        music.playSoundEffect(music.builtinSoundEffect(soundExpression.twinkle), SoundExpressionPlayMode.UntilDone)
     }
-    basic.pause(1000)
-    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+}
+input.onButtonPressed(Button.AB, function () {
+    Num = 0
+    Object_Classification()
 })
+input.onButtonPressed(Button.B, function () {
+    Light()
+})
+input.onGesture(Gesture.LogoUp, function () {
+    music.startMelody(music.builtInMelody(Melodies.PowerDown), MelodyOptions.Once)
+    basic.showString("Hello!")
+})
+function Good () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(1000)
+    basic.showString("Good Choice")
+}
+let Num = 0
 let temperature = 0
-basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.Happy)
+basic.showString("Hello!")
 basic.forever(function () {
     temperature = input.temperature()
 })
